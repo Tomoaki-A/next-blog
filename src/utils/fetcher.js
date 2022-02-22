a563031d258099c891d5d5095d48b9fa010ec826
任意のユーザ名 <exsrx97@gmail.com>
2022-02-22 22:56:12 +0900

    cash

--- src/utils/fetcher.js
+++
@@ -1,8 +0,0 @@
-export const fetcher = async (url) => {
-  const res = await fetch(url);
-  if (!res.ok) {
-    throw new Error(`Could not fetch ${url}, received ${res.status}`);
-  }
-  const json = await res.json();
-  return json;
-};
